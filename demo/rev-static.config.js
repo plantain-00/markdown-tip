ee834e0be6879b3f6a4e14ef3f260c9c694ef919
york yao <yaoao12306@outlook.com>
2017-07-07 07:17:15 +0800

    add hash for dynamic imported files

--- demo/rev-static.config.js
+++ demo/rev-static.config.js
@@ -1,21 +1,21 @@
 module.exports = {
   inputFiles: [
-    'demo/**/index.bundle.js',
+    'demo/**/index.bundle-*.js',
+    'demo/**/*.index.bundle-*.js',
     'demo/**/index.ejs.html',
     'demo/*.bundle.css',
     'demo/*.png'
   ],
+  revisedFiles: [
+    'demo/**/index.bundle-*.js',
+    'demo/**/*.index.bundle-*.js'
+  ],
   outputFiles: file => file.replace('.ejs', ''),
-  json: false,
   ejsOptions: {
     rmWhitespace: true
   },
   sha: 256,
   customNewFileName: (filePath, fileString, md5String, baseName, extensionName) => baseName + '-' + md5String + extensionName,
-  noOutputFiles: [],
-  es6: false,
-  less: false,
-  scss: false,
   base: 'demo',
   fileSize: 'demo/file-size.json'
 }
