5c69f48cca03492ea2e8d5e4cab8828722adcfb5
york yao <yaoao12306@outlook.com>
2017-10-12 14:57:07 +0800

    improve test

--- screenshots/index.ts
+++ screenshots/index.ts
@@ -7,7 +7,14 @@ import * as puppeteer from "puppeteer";
 
     for (const type of ["vue", "react", "angular"]) {
         await page.goto(`http://localhost:8000/demo/${type}`);
+        await page.waitFor(100);
         await page.screenshot({ path: `screenshots/${type}-initial.png`, fullPage: true });
+        const elements = await page.$$(".markdown-tip-item-text");
+        for (let i = 0; i < elements.length; i++) {
+            await elements[i].hover();
+            await page.waitFor(100);
+            await page.screenshot({ path: `screenshots/${type}-hover-${i}.png`, fullPage: true });
+        }
     }
 
     browser.close();
