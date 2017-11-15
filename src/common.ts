48468b61a78d6b9147ff1c7a6f76e06222ff6fff
york yao <yaoao12306@outlook.com>
2017-11-15 10:46:47 +0800

    export defaultLocale; 5.0.1

--- src/common.ts
+++ src/common.ts
@@ -5,7 +5,10 @@ export type Data = {
 
 export type Locale = Data[];
 
-const defaultLocale: Locale = [
+/**
+ * @public
+ */
+export const defaultLocale: Locale = [
     {
         text: "Heading",
         title: `# h1 Heading
