c277b3986457cbd1f914aba2f3af15b3e40cf324
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-30 15:34:22 +0000

    Deploy plantain-00/markdown-tip to github.com/plantain-00/markdown-tip.git:gh-pages

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -74,7 +74,8 @@ module.exports = {
     less: `stylelint ${lessFiles}`,
     export: `no-unused-export ${tsFiles} --exclude ${excludeTsFiles}`,
     commit: `commitlint --from=HEAD~1`,
-    markdown: `markdownlint README.md`
+    markdown: `markdownlint README.md`,
+    typeCoverage: 'lerna exec -- type-coverage -p src --at-least 84'
   },
   test: [
     'tsc -p spec',
