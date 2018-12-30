483750aca5f163edaa9dd55860a0104f8c00addb
york yao <yaoao12306@outlook.com>
2018-12-30 08:07:58 +0800

    chore: update dependencies

--- packages/vue/src/rollup.config.js
+++ packages/vue/src/rollup.config.js
@@ -4,13 +4,13 @@ import commonjs from 'rollup-plugin-commonjs'
 
 export default {
   input: 'packages/vue/dist/index.js',
-  name: 'MarkdownTip',
   plugins: [
     resolve({ browser: true }),
     uglify(),
     commonjs()
   ],
   output: {
+    name: 'MarkdownTip',
     file: 'packages/vue/dist/markdown-tip-vue.min.js',
     format: 'umd'
   },
