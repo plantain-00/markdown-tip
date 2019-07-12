ef21fe0c787f87fbf1ece84d99c5e6310927a38b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-12 22:47:31 +0000

    Deploy plantain-00/markdown-tip to github.com/plantain-00/markdown-tip.git:gh-pages

--- packages/vue/src/variables.ts
+++ packages/vue/src/variables.ts
@@ -3,10 +3,12 @@
  * It is not mean to be edited by hand
  */
 // tslint:disable
+/* eslint-disable */
 import { MarkdownTip } from "./index"
 
 // @ts-ignore
 export function indexTemplateHtml(this: MarkdownTip) {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"markdown-tip"},_vm._l((_vm.datas),function(data){return _c('div',{key:data.text,staticClass:"markdown-tip-item",on:{"mouseenter":function($event){return _vm.mouseenter(data, $event)},"mouseleave":function($event){return _vm.mouseleave(data)}}},[_c('span',{staticClass:"markdown-tip-item-text"},[_vm._v(_vm._s(data.text))]),_vm._v(" "),(data.text === _vm.visibleText)?_c('pre',{class:_vm.titleClass},[_vm._v(_vm._s(data.title))]):_vm._e()])}),0)}
 // @ts-ignore
 export var indexTemplateHtmlStatic = [  ]
+/* eslint-enable */
 // tslint:enable
